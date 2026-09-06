445e151e67d3402f791a083867779323e06bda0d
uauz1 <uauz99@gmail.com>
2026-09-06 21:56:22 +0300

    Enable contextual shortcuts and smoother chat UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './ios-audio-unlock.js';
+import './smart-options.js';
 import App from './App.jsx';
 import './styles.css';
+import './smart-options.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
