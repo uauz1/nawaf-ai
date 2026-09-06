ef5e94370ed1948aecc25f87a781241084bf05e5
uauz1 <uauz99@gmail.com>
2026-09-06 19:41:57 +0300

    Load iOS audio unlock before app

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './ios-audio-unlock.js';
 import App from './App.jsx';
 import './styles.css';
 import './api-key.css';
