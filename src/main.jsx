dc5abb6a48cc538ccc75a6710a0640bd80d4212f
uauz1 <uauz99@gmail.com>
2026-09-06 14:22:12 +0300

    revert: restore pre-animation dashboard entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import LivingMascotLayer from './LivingMascotLayer.jsx';
 import './styles.css';
-import './living-mascot-v2.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <LivingMascotLayer />
   </React.StrictMode>
 );
