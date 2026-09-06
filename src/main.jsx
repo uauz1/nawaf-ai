937ba92d2f6d1baf404a5e73fee6fb02955ead5a
uauz1 <uauz99@gmail.com>
2026-09-06 20:44:03 +0300

    Enable robust iPhone voice playback patch

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './ios-audio-unlock.js';
+import './ios-speech-fix.js';
 import App from './App.jsx';
 import './styles.css';
 
